7d89f3b011a312f418c3a8a7880ffeebcd5c2e93
CJ_Link <99633691+CJL1nk@users.noreply.github.com>
2026-02-17 00:59:20 -0800

    cool awesome example

--- src/main.c
+++ src/main.c
@@ -6,9 +6,17 @@
 
 int main() {
 
-    const AudioDescriptor* audio = init_standard_file("rard.wav", 1.f);
+    const AudioDescriptor* audio = init_standard_file("rard.wav", 4.f);
 
-    write_notes(audio, 1.0f, 4, C5, E5, G5, B5);
+    write_notes(audio, 0.5f, 2, A5, E5);
+    write_note(audio, A4, 0.5f);
+    write_note(audio, E5, 0.5f);
+    write_note(audio, A4, 0.5f);
+
+    write_notes(audio, 0.5f, 2, A5, E5);
+    write_note(audio, A4, 0.5f);
+    write_note(audio, E5, 0.5f);
+    write_note(audio, D5, 0.5f);
 
     fclose(audio->file);
 
